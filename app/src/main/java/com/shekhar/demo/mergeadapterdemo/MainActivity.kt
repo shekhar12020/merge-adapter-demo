dd7b4015cfbbfdd22d5b2b213e11f0713a88bcd0
Shekhar Sahu <chandrashekhar@joshsoftware.com>
2020-04-10 13:29:01 +0530

    Initial commit

---
+++ app/src/main/java/com/shekhar/demo/mergeadapterdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shekhar.demo.mergeadapterdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
